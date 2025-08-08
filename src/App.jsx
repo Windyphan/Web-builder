2f57b4ae56d8456b04f2af962a2f3931fdb6bde0
Windyphan <thandongdatviettatca@gmail.com>
2025-08-08 19:26:10 +0700

    Change HashRouter to BrowserRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { HashRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import { ThemeProvider } from './contexts/ThemeContext';
 import Home from './pages/Home';
 import Portfolio from './pages/Portfolio';
