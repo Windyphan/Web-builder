b5404d8c687cfa92adfe34618515ef53c44b8f91
Windyphan <thandongdatviettatca@gmail.com>
2025-10-03 18:38:50 +0100

    Remove video section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react';
 import Header from '../components/Header';
 import Hero from '../components/Hero';
-import VideoSection from '../components/VideoSection';
 import InkFillSection from '../components/InkFillSection';
 import Services from '../components/Services';
 import TrustedClients from '../components/TrustedClients';
@@ -33,7 +32,6 @@ function Home() {
         }`}>
             <Header />
             <Hero />
-            <VideoSection />
             <InkFillSection />
             <TrustedClients />
             <Services />
