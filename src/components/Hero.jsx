8138dd946112cd0b33025f0a1e5260459e0856a9
Windyphan <thandongdatviettatca@gmail.com>
2025-10-03 15:54:19 +0100

    Add video in background

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -19,18 +19,16 @@ const Hero = () => {
             style={{ minHeight: '100vh' }}
         >
             {/* Vimeo Background */}
-            <div className="absolute inset-0 z-0">
+            <div className="absolute inset-0 z-0" style={{ pointerEvents: 'none' }}>
                 <div style={{padding: '56.25% 0 0 0', position: 'relative', width: '100%', height: '100%'}}>
                     <iframe
-                        src="https://player.vimeo.com/video/1124235622?badge=0&autopause=0&player_id=0&app_id=58479&autoplay=1&loop=1&muted=1"
+                        src="https://player.vimeo.com/video/1124235622?background=1"
                         frameBorder="0"
-                        allow="autoplay; fullscreen; picture-in-picture; clipboard-write; encrypted-media; web-share"
-                        referrerPolicy="strict-origin-when-cross-origin"
+                        allow="autoplay; fullscreen; picture-in-picture"
                         style={{position: 'absolute', top: 0, left: 0, width: '100%', height: '100%'}}
-                        title="1350205-hd_1920_1080_30fps"
+                        title="background video"
                     ></iframe>
                 </div>
-                <script src="https://player.vimeo.com/api/player.js"></script>
             </div>
 
             {/* Main Content */}
