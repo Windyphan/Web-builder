efebc39b1d778b421b0f509c6c3b5c645d98808d
Windyphan <thandongdatviettatca@gmail.com>
2025-10-27 14:38:08 +0000

    Remove Services quick link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,7 +8,6 @@ const Footer = () => {
     const quickLinks = [
         { name: 'Home', href: '/' },
         { name: 'About', href: '/about' },
-        { name: 'Services', href: '/#services' },
         { name: 'Portfolio', href: '/portfolio' },
         { name: 'Contact', href: '/contact' }
     ];
