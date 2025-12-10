151005231c0db816e248c6f9bcf61749fe8f9041
Windyphan <thandongdatviettatca@gmail.com>
2025-12-10 13:28:43 +0000

    Remove debug

--- src/App.jsx
+++ src/App.jsx
@@ -70,7 +70,6 @@ function App() {
         <ErrorBoundary>
             <ThemeProvider>
                 <Router>
-                    <RouteDebugger />
                     <ScrollToTop />
                     <CanonicalURL />
                     <div className="min-h-screen bg-white dark:bg-gray-900 text-gray-900 dark:text-gray-100 transition-colors duration-300">
